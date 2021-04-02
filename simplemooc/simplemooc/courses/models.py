6d5cf890a85ac0685ca13a809fc3194af202dd40
NatalNW <natanaelweslley1@gmail.com>
2021-04-02 01:42:37 -0300

    working with course model admin

--- simplemooc/simplemooc/courses/models.py
+++ simplemooc/simplemooc/courses/models.py
@@ -1,10 +1,19 @@
 from django.db import models
 
+
 class Course(models.Model):
     name = models.CharField('Name', max_length=100)
-    slug = models.SlugField('Atalho')
+    slug = models.SlugField('Shortcut')
     description = models.TextField('Description', blank=True)
     start_date = models.DateField('Start Date', null=True, blank=True)
     image = models.ImageField(upload_to='courses/images', verbose_name='Image', null=True, blank=True)
-    created_at =models.DateTimeField('Created At', auto_now_add=True)
+    created_at = models.DateTimeField('Created At', auto_now_add=True)
     updated_at = models.DateTimeField('Updated At', auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Course'
+        verbose_name_plural = 'Courses'
+        ordering = ['name']
