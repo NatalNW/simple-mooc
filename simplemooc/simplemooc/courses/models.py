e813a63e19ed5981725fb2826060b37dc610bec5
NatalNW <natanaelweslley1@gmail.com>
2021-04-02 01:07:36 -0300

    create courses model

--- simplemooc/simplemooc/courses/models.py
+++ simplemooc/simplemooc/courses/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Course(models.Model):
+    name = models.CharField('Name', max_length=100)
+    slug = models.SlugField('Atalho')
+    description = models.TextField('Description', blank=True)
+    start_date = models.DateField('Start Date', null=True, blank=True)
+    image = models.ImageField(upload_to='courses/images', verbose_name='Image', null=True, blank=True)
+    created_at =models.DateTimeField('Created At', auto_now_add=True)
+    updated_at = models.DateTimeField('Updated At', auto_now=True)
--- simplemooc/simplemooc/settings.py
+++ simplemooc/simplemooc/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'simplemooc.core',
+    'simplemooc.courses',
 ]
 
 MIDDLEWARE = [
