cdc75f2caef834e121eb965a84ecf2256383e8c4
NatalNW <natanaelweslley1@gmail.com>
2021-04-06 22:07:49 -0300

    working with course's image and add media folder

--- simplemooc/simplemooc/urls.py
+++ simplemooc/simplemooc/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
+from django.conf import settings
+from django.conf.urls.static import static
 # from simplemooc.core import urls
 # from simplemooc.courses import urls
 
@@ -24,3 +26,6 @@ urlpatterns = [
     path('courses/', include(('simplemooc.courses.urls', 'courses'), namespace='courses')),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
