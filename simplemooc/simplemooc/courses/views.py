8009f89b644242d43a72fcf00bdabf65187c486d
NatalNW <natanaelweslley1@gmail.com>
2021-04-07 23:46:05 -0300

    create contact form in details course page

--- simplemooc/simplemooc/courses/views.py
+++ simplemooc/simplemooc/courses/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 from .models import Course
-
+from .forms import ContactCourse
 
 def index(request):
     courses = Course.objects.all()
@@ -14,9 +14,16 @@ def index(request):
 
 def details(request, slug):
     course = get_object_or_404(Course, slug=slug)
-    context = {
-        'course': course
-    }
+    context = {}
+    if request.method == 'POST':
+        form = ContactCourse(request.POST)  
+        if form.is_valid():
+            context['is_valid'] = True
+            form = ContactCourse()
+    else:
+        form = ContactCourse()
+    context['course'] = course
+    context['form'] = form
     template_name = 'courses/details.html'
     return render(request, template_name, context)
 
