eeae70d01b6fcc177139ba5aaceb70e729ff9cd2
NatalNW <natanaelweslley1@gmail.com>
2021-08-10 21:29:50 -0300

    create register page

--- simplemooc/accounts/urls.py
+++ simplemooc/accounts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
-from django.contrib.auth import views
-from django.views.generic.base import TemplateView
+from django.contrib.auth import views as auth_views
+from simplemooc.accounts import views
 
 urlpatterns = [
-    path('login/', views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('register/', views.register, name='register'),
 ]
--- simplemooc/accounts/views.py
+++ simplemooc/accounts/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth.forms import UserCreationForm
+from django.conf import settings
 
-# Create your views here.
+def register(req):
+    template_name = 'register.html'
+
+    if req.method == 'POST':
+        form = UserCreationForm(req.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(settings.LOGIN_URL)
+    else:
+        form = UserCreationForm()
+
+    context = {
+        'form': form
+    }
+
+    return render(req, template_name, context)
+
