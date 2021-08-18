df11a92a5956594822d83763ec45465b09b251ed
NatalNW <natanaelweslley1@gmail.com>
2021-08-17 22:53:02 -0300

    unique email for register

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,9 +1,18 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 class RegisterForm(UserCreationForm):
 
     email = forms.EmailField(label='E-mail') 
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+
+        if User.objects.filter(email=email).exists():
+                raise forms.ValidationError('Email already being used!')
+        
+        return email
 
     def save(self):
         user = super(RegisterForm, self).save(commit=False)
